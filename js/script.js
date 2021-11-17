45d2b437f9bb975219f10e8be66a6da44c491f35
painteyes <andreantoniocchipinti@gmail.com>
2021-11-17 15:51:55 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Mail
+
+// Chiedi all’utente la sua email,
+let userEmail = 
+
+// controlla che sia nella lista di chi può accedere,
+
+
+// stampa un messaggio appropriato sull’esito del controllo.
+
