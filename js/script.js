b7bd9a2515d7a252b8ddceaa1e5e4c5d5423c2c4
painteyes <andreantoniocchipinti@gmail.com>
2021-11-17 16:14:23 +0100

    commit changes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // Mail
 
-// let fakeEmailDatabase = ['liberodiessere@libero.it','diavoletta87@hotmail.it','fidab54345@erpipo.com'];
+let fakeEmailDatabase = ['liberodiessere@libero.it','diavoletta87@hotmail.it','fidab54345@erpipo.com'];
 
-// // Chiedi all’utente la sua email,
-// let userEmail = prompt('Inserisci la tua email');
+// Chiedi all’utente la sua email,
+let userEmail = prompt('Inserisci la tua email');
 
-// // controlla che sia nella lista di chi può accedere,
-// for (i=0 ; i < fakeEmailDatabase.length ; i++) {
-//     // stampa un messaggio appropriato sull’esito del controllo.
-//     let thisEmail = fakeEmailDatabase[i];
-//     if (userEmail === thisEmail){
-//         console.log ('La tua email è nel database');
-//     }
-// }
+// controlla che sia nella lista di chi può accedere,
+for (i=0 ; i < fakeEmailDatabase.length ; i++) {
+    // stampa un messaggio appropriato sull’esito del controllo.
+    let thisEmail = fakeEmailDatabase[i];
+    if (userEmail === thisEmail){
+        console.log ('La tua email è nel database');
+    }
+}
 
 // Gioco dei dadi
 
