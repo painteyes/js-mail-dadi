4af3c61a5a8c8bf91f3e20ba6ba1f0b8432816f4
painteyes <andreantoniocchipinti@gmail.com>
2021-11-17 16:16:08 +0100

    commit changes

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -1,19 +1,3 @@
-// Mail
-
-let fakeEmailDatabase = ['liberodiessere@libero.it','diavoletta87@hotmail.it','fidab54345@erpipo.com'];
-
-// Chiedi all’utente la sua email,
-let userEmail = prompt('Inserisci la tua email');
-
-// controlla che sia nella lista di chi può accedere,
-for (i=0 ; i < fakeEmailDatabase.length ; i++) {
-    // stampa un messaggio appropriato sull’esito del controllo.
-    let thisEmail = fakeEmailDatabase[i];
-    if (userEmail === thisEmail){
-        console.log ('La tua email è nel database');
-    }
-}
-
 // Gioco dei dadi
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
