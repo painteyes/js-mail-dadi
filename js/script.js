6289c67f6a8caadc25aa5ace14f79a6afd6d3d4f
painteyes <andreantoniocchipinti@gmail.com>
2021-11-17 16:05:51 +0100

    commit changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 // Mail
-
+let fakeEmailDatabase = ['liberodiessere@libero.it','diavoletta87@hotmail.it','fidab54345@erpipo.com'];
 // Chiedi all’utente la sua email,
-let userEmail = 
+let userEmail = prompt('Inserisci la tua email');
 
 // controlla che sia nella lista di chi può accedere,
+for (i=0 ; i < fakeEmailDatabase.length ; i++) {
+    
+    if (userEmail === fakeEmailDatabase[i]){
+        console.log ('La tua email è nel database');
+    }
 
+}
 
 // stampa un messaggio appropriato sull’esito del controllo.
 
