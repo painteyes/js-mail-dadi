f6000d6e49b299f66b52d31bcbac2311f04c2073
painteyes <andreantoniocchipinti@gmail.com>
2021-11-18 13:46:27 +0100

    commit changes

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,21 +1,27 @@
 // Mail
-
 let fakeEmailDatabase = ['liberodiessere@libero.it','diavoletta87@hotmail.it','fidab54345@erpipo.com'];
 
 // Chiedi all’utente la sua email,
 let userEmail = prompt('Inserisci la tua email');
 
 // controlla che sia nella lista di chi può accedere,
-for (let i=0 ; i < fakeEmailDatabase.length ; i++) {
+let emailFound = false;
 
-    // stampa un messaggio appropriato sull’esito del controllo.
-    let thisEmail = fakeEmailDatabase[i];
-
-    if (userEmail === thisEmail){
-        alert  ('La tua email è nel database');
-    }   
+for (let i=0 ; i < fakeEmailDatabase.length  ; i++) {
+    
+    const thisEmail = fakeEmailDatabase[i];
+    
+    if (userEmail === thisEmail) {
+        emailFound = true;
+    }
 }
 
-alert  ('Mi dispiace, la tua email non è nel database');
-    
+// stampa un messaggio appropriato sull’esito del controllo.
+if (emailFound) {
+    alert  ('La tua email è nel database'); 
+} else {
+    alert  ('Mi dispiace, la tua email non è nel database');
+}
+
+
 
